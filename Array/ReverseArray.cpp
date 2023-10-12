97bd4e20f50a8cb042510ba62dd72e54d2794f3a
PRAKALP-PANDE <prakalpsp16@gmail.com>
2023-10-12 17:08:58 +0530

    Reverse an array

---
+++ Array/ReverseArray.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int arr[], int start, int end)
+{
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void printingarray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printingarray(arr, n);
+    cout << "After Reversing array:" << endl;
+    reverse(arr, 0, n - 1);
+    printingarray(arr, n);
+    return 0;
+}
