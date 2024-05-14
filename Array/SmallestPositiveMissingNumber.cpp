b774eced7edbad91375b296b201f3421bff4f511
PRAKALP-PANDE <prakalpsp16@gmail.com>
2024-05-14 23:43:26 +0530

    Array

---
+++ Array/SmallestPositiveMissingNumber.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    const int N = 1e6 + 2;
+    bool check[N];
+    for (int i = 0; i < N; i++)
+    {
+        check[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= 0)
+        {
+            check[arr[i]] = 1;
+        }
+    }
+    int ans = -1;
+    for (int i = 1; i < N; i++)
+    {
+        if (check[i] == 0)
+        {
+            ans = i;
+            break;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
